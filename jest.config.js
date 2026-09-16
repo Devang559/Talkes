58fc666870537038bb43f40a5ce8ac53b86f34fd
Devang Sharma <devangsharma14feb@gmail.com>
2026-09-16 18:47:18 +0530

    first commit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   setupFiles: ['<rootDir>/jest/setup.js'],
+  testEnvironment: 'node',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-ble-plx|@react-native-async-storage)/)',
+  ],
 };
--- jest/setup.js
+++ jest/setup.js
@@ -20,3 +20,64 @@ jest.mock('@react-native-async-storage/async-storage', () => ({
     getAllKeys: jest.fn(async () => Object.keys(storage)),
   },
 }));
+
+jest.mock('react-native-ble-plx', () => {
+  class BleManager {
+    state() {
+      return Promise.resolve('PoweredOn');
+    }
+    onStateChange(callback, emitCurrent) {
+      if (emitCurrent) {
+        callback('PoweredOn');
+      }
+      return { remove() {} };
+    }
+    startDeviceScan() {
+      return Promise.resolve();
+    }
+    stopDeviceScan() {
+      return Promise.resolve();
+    }
+    connectToDevice() {
+      return Promise.resolve({
+        id: 'test-device-id',
+        rssi: -60,
+        mtu: 23,
+        name: 'TestDevice',
+        connect: jest.fn(),
+        cancelConnection: jest.fn(),
+        discoverAllServicesAndCharacteristics: jest.fn(() => Promise.resolve()),
+        requestMTU: jest.fn(() => Promise.resolve({ mtu: 23 })),
+        monitorCharacteristicForService: jest.fn(),
+        writeCharacteristicWithoutResponseForService: jest.fn(() => Promise.resolve()),
+        onDisconnected: jest.fn(),
+      });
+    }
+    cancelDeviceConnection() {
+      return Promise.resolve();
+    }
+    devices() {
+      return Promise.resolve([]);
+    }
+    connectedDevices() {
+      return Promise.resolve([]);
+    }
+    destroy() {
+      return Promise.resolve();
+    }
+  }
+
+  return {
+    BleManager,
+    State: {
+      Unknown: 'Unknown',
+      Resetting: 'Resetting',
+      Unsupported: 'Unsupported',
+      Unauthorized: 'Unauthorized',
+      PoweredOff: 'PoweredOff',
+      PoweredOn: 'PoweredOn',
+    },
+    Device: jest.fn(),
+    Subscription: jest.fn(),
+  };
+});
